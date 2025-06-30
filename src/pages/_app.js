dbc02419e5d87cf1c4a261e194c0a37346a8106d
ninedentalcentre <ninedentalcentre@gmail.com>
2025-06-30 13:49:27 +0530

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,7 @@
-// src/pages/_app.js
-import Head from 'next/head';
+import '../css/style.css';
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <link rel="icon" href="/images/favicon.png" type="image/png" />
-        <title>Nine Dental Centre</title> {/* optional */}
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
